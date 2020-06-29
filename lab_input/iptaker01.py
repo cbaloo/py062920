965cf9b3f0ea86ae8f476ae3499a9fb58a585405
Chandana Baloo <student@pyb-8341-bchd.localdomain>
2020-06-29 17:00:21 -0400

    Practice labs for Python

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def main():
+
+    user = input("Please enter an IPv4 IP address:")
+    print("You told me the IPv4 address is:" + user)
+
+
+if __name__ == "__main__": main()
+
+
+
+
