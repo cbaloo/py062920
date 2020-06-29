fc5cc05ceab03bb8a9d7ae6be80a5b73946bab59
Chandana Baloo <chandanabaloo@gmail.com>
2020-06-29 18:14:45 -0400

    end of day 1

--- lab_input/iptaker01.py
+++ lab_input/iptaker01.py
@@ -4,6 +4,8 @@ def main():
 
     user = input("Please enter an IPv4 IP address:")
     print("You told me the IPv4 address is:" + user)
+    vendor = input("Enter the vendor name associated with the device:")
+    print("Vendor name please:" + vendor)
 
 
 if __name__ == "__main__": main()
