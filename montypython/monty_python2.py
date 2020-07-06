ce42f790f79ad1f7668c99229ed2662984630ab5
Chandana Baloo <chandanabaloo@gmail.com>
2020-07-06 08:20:11 -0400

    while looping

--- montypython/monty_python2.py
+++ montypython/monty_python2.py
@@ -2,7 +2,7 @@
 
 
 def main():
-
+    """
     round = 0
     while True:
         round = round + 1
@@ -16,4 +16,20 @@ def main():
            break             # break statement escapes the while loop
     else:                 # if answer was wrong, and round is not yet equal to 3
           print('Sorry. Try again!')
-                                                                                         if __name__ == "__main__": main()        
+
+    """
+
+
+round = 0
+answer = " "
+while round < 3 and answer != "Brian":
+    round += 1     # increase the round counter by 1
+    answer = input('Finish the movie title, "Monty Python\'s The Life of ______": ')
+    if answer == "Brian": # logic to check if user gave correct answer
+        print("Correct!")
+    elif round == 3:    # logic to ensure round has not yet reached 3
+        print("Sorry, the answer was Brian.")
+    else:                 # if answer was wrong
+        print("Sorry. Try again!") 
+    if __name__ == "__main__":
+        main()        
