8367440a50688b529b3bf1a041fab862ed7d94d2
cbaloo <cb141504@gmail.com>
2020-07-11 03:32:00 -0700

    dulicate files deleted

--- input.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-""" input() practice """
-user = input ("Enter your name:")
-user1 = input("Enter your age:")
-
-print(user+"\n "+user1)
-
--- lists8.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-""" practice mixed lists"""
-
-mylists = [ 443,"172.20.200.37","port", "sss"]
-
-print(" My ip address is :" +  mylists[1] + "\n" " I'm using Https port:" + str(mylists[0]))
-
